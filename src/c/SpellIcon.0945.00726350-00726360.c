c895edc27efc0543900e6e4b13f91cc01a48e1ec
Sandy Carter <bwrsandman@gmail.com>
2024-12-28 21:00:28 -0500

    SpellIcon: Switch asm function for inline asm in C

---
+++ src/c/SpellIcon.0945.00726350-00726360.c
@@ -0,0 +1,14 @@
+__attribute__((naked)) void GetSpellSeedInfo__9SpellIconCFv(void)
+{
+    __asm__ volatile ("{disp32} mov       eax, dword ptr [ecx + 0x00000080]"); // 0x00726350    8b8180000000
+    __asm__ volatile ("ret");                                                  // 0x00726356    c3
+    __asm__ volatile ("nop");                                                  // 0x00726357    90
+    __asm__ volatile ("nop");                                                  // 0x00726358    90
+    __asm__ volatile ("nop");                                                  // 0x00726359    90
+    __asm__ volatile ("nop");                                                  // 0x0072635a    90
+    __asm__ volatile ("nop");                                                  // 0x0072635b    90
+    __asm__ volatile ("nop");                                                  // 0x0072635c    90
+    __asm__ volatile ("nop");                                                  // 0x0072635d    90
+    __asm__ volatile ("nop");                                                  // 0x0072635e    90
+    __asm__ volatile ("nop");                                                  // 0x0072635f    90
+}
