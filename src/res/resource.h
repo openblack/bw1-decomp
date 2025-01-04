3faf2a487043cc02b582e6462310d865e00ef4a9
Sandy Carter <bwrsandman@gmail.com>
2025-01-03 23:16:51 -0500

    rsrc: Compile .rsrc section from .rc file using llvm-rc

---
+++ src/res/resource.h
@@ -0,0 +1,20 @@
+#define VS_VERSION_INFO                   1
+
+#define IDD_LIBSDLG                       102
+
+#define IDB_BMP1                          102
+#define IDB_BMP2                          106
+
+#define IDC_CURSOR                        108
+
+#define IDC_CMB1                          1003
+#define IDC_LSB1                          1004
+#define IDC_HOSTBUTTON                    1005
+#define IDC_JOINBUTTON                    1006
+#define IDC_EDT1                          1017
+
+#define ICM_FPS                           40003
+#define ICM_PAUSE                         40016
+#define ICM_LOG_PROPERTIES                40020
+#define ICM_DUMP_BITMAP                   40024
+#define ICM_FULLSCREEN                    40026
