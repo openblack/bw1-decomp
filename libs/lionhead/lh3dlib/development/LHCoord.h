485a16cc52de81c8ca023e0f9c5f5d9016f0d00b
Sandy Carter <bwrsandman@gmail.com>
2025-01-17 22:53:14 -0500

    Reverse ProcessGraphicsEngine__5GGameFUlUl

--- libs/lionhead/lh3dlib/development/LHCoord.h
+++ libs/lionhead/lh3dlib/development/LHCoord.h
@@ -13,6 +13,17 @@ static_assert(sizeof(struct LHCoord) == 0x8, "Data type is of wrong size");
 // Constructors
 
 // win1.41 inlined mac 1006ebc0 LHCoord::LHCoord(LHCoord const &)
-void __fastcall __ct__7LHCoordFRC7LHCoord(struct LHCoord* this, const void* edx, const struct LHCoord* other);
+inline void __fastcall __ct__7LHCoordFRC7LHCoord(struct LHCoord* this, const void* edx, const struct LHCoord* other)
+{
+    __asm__ (
+        "mov ecx, dword ptr [%1 + 4]\n"
+        "mov eax, dword ptr [%1]\n"
+        "mov dword ptr [%0], eax\n"
+        "mov dword ptr [%0 + 4], ecx\n"
+        :
+        : "i" (this), "i" (other)
+        : "eax", "ecx", "memory"
+    );
+}
 
 #endif /* BW1_DECOMP_LH_COORD_INCLUDED_H */
