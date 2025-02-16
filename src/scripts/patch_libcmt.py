7365f3147faf6703ab91eaff5caefa882ebc4f8b
Sandy Carter <bwrsandman@gmail.com>
2025-02-16 17:36:10 -0500

    link with longjmp.obj from LIBCMT.LIB

--- src/scripts/patch_libcmt.py
+++ src/scripts/patch_libcmt.py
@@ -1,10 +1,21 @@
-import sys
+from pathlib import Path
 
-with open(sys.argv[1], 'rb') as fin:
-    data = bytearray(fin.read())
 
-# IMAGE_SCN_TYPE_NO_PAD
-data[0x38] |= 0x8
+def patch_libcmt(input_file: Path, output_file: Path, do_patching: bool):
+    with open(input_file, 'rb') as fin:
+        data = bytearray(fin.read())
 
-with open(sys.argv[2], 'wb') as fout:
-    fout.write(data)
+    if do_patching:
+        # IMAGE_SCN_TYPE_NO_PAD
+        data[0x38] |= 0x8
+
+    with open(output_file, 'wb') as fout:
+        fout.write(data)
+
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) != 4:
+        print(f"Usage: {sys.argv[0]} <input_file> <output_file> <do_patching>")
+        sys.exit(1)
+    patch_libcmt(Path(sys.argv[1]), Path(sys.argv[2]), sys.argv[3] == 'ON')
