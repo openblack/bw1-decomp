63f4589d4e4296d1aa0bbd1a825460b65575820c
Sandy Carter <bwrsandman@gmail.com>
2025-02-22 16:43:19 -0500

    link with wcspbrk, wcsspn, wcsrchr, wcschr, strspn

--- src/scripts/post_assemble_patch.py
+++ src/scripts/post_assemble_patch.py
@@ -96,9 +96,10 @@ def patch_black(input_path: Path, output_path: Path, turn_off_fullscreen: bool, 
             (0xff8, "<"),
             (0xffc, "4"),
             # libcmt padding is 0xCC
-            (0x004A645B, b"\xCC"),
-            (0x004A6281, b"\xCC\xCC\xCC"),
-            (0x004A62FD, b"\xCC\xCC\xCC"),
+            (0x004A645B, b"\xCC" * 1),
+            (0x004A6281, b"\xCC" * 3),
+            (0x004A62FD, b"\xCC" * 3),
+            (0x004A6057, b"\xCC" * 9),
             # Garbage string in the rsrc
             (0x008428F8, "property of their respective owners".encode("utf-16-le")),
         ]
