c071471e93016f1f35e47a3baf204a3f7c1f0e1b
Sandy Carter <bwrsandman@gmail.com>
2025-02-23 00:01:19 -0500

    clean-up dll jump tables

---
+++ src/asm/unprocessed/runblack.reassemble.1342.008a5b00-008a5b0c-dll-jmp-table.asm
@@ -0,0 +1,11 @@
+.intel_syntax noprefix
+.balign 0
+
+.extern __imp__DirectInputCreateA@4
+.extern __imp__LHWaveGetQMixerDirectSoundObject_LH_AudioSystem__QAEPAXXZ@4
+
+.globl _jmp_addr_0x008a5b00
+
+_jmp_addr_0x008a5b00:
+                         jmp              dword ptr [__imp__DirectInputCreateA@4]                                         // 0x008a5b00    ff2534908a00
+                         jmp              dword ptr [__imp__LHWaveGetQMixerDirectSoundObject_LH_AudioSystem__QAEPAXXZ@4]  // 0x008a5b06    ff25c4968a00
