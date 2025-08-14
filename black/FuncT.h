9c8fa30e94b1e814454aae0dce779f92d9c9ecff
Sandy Carter <bwrsandman@gmail.com>
2025-08-13 21:26:46 -0400

    Add FuncT class family

---
+++ black/FuncT.h
@@ -0,0 +1,24 @@
+#ifndef BW1_DECOMP_FUNC_T_INCLUDED_H
+#define BW1_DECOMP_FUNC_T_INCLUDED_H
+
+#include <assert.h> /* For static_assert */
+#include <stddef.h> /* For size_t */
+#include <stdint.h> /* For uint32_t, uint8_t */
+
+// Forward Declares
+
+struct FuncT;
+
+struct FuncTVftable
+{
+  void (__fastcall* Eval)(const struct FuncT* this, const void* edx, float param_1, float* param_2, float* param_3);  /* 0x0 */
+};
+static_assert(sizeof(struct FuncTVftable) == 0x4, "Data type is of wrong size");
+
+struct FuncT
+{
+  struct FuncTVftable* vftable;  /* 0x0 */
+};
+static_assert(sizeof(struct FuncT) == 0x4, "Data type is of wrong size");
+
+#endif /* BW1_DECOMP_FUNC_T_INCLUDED_H */
