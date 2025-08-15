7571ae50300eabc388dfdf758270db993f3defea
Sandy Carter <bwrsandman@gmail.com>
2025-08-14 21:15:39 -0400

    SecretCreature: Add definition

---
+++ black/SecretCreature.h
@@ -0,0 +1,25 @@
+#ifndef BW1_DECOMP_SECRET_CREATURE_INCLUDED_H
+#define BW1_DECOMP_SECRET_CREATURE_INCLUDED_H
+
+#include <assert.h> /* For static_assert */
+
+// Forward Declares
+
+struct SecretCreature;
+
+struct SecretCreatureVftable
+{
+  void (__fastcall* __dt)(struct SecretCreature* this);  /* 0x0 */
+};
+static_assert(sizeof(struct SecretCreatureVftable) == 0x4, "Data type is of wrong size");
+
+struct SecretCreature
+{
+  struct SecretCreatureVftable* vftable;  /* 0x0 */
+};
+static_assert(sizeof(struct SecretCreature) == 0x4, "Data type is of wrong size");
+
+// win1.41 0095726c mac 1075ac64 SecretCreature::`vftable'
+extern const struct SecretCreatureVftable __vt__14SecretCreature asm("??_7SecretCreature@@6B@");
+
+#endif /* BW1_DECOMP_SECRET_CREATURE_INCLUDED_H */
