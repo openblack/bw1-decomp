fd4583efc5d844c7cb7c3146b30002a765408b5a
Sandy Carter <bwrsandman@gmail.com>
2025-09-07 20:09:35 -0400

    LoaderAnon: Fix style

--- black/LoaderAnon.h
+++ black/LoaderAnon.h
@@ -17,9 +17,7 @@ struct LoaderAnon
 };
 static_assert(sizeof(struct LoaderAnon) == 0x4, "Data type is of wrong size");
 
-// Static methods
-
 // win1.41 00433f00 mac 10185870 LoadIt<GVillagerStateTableInfo>(LoaderAnon *, char *, GVillagerStateTableInfo *, unsigned long, bool, LHFile *)
-void LoadIt__FP10LoaderAnonPcP23GVillagerStateTableInfoUlbP6LHFile(struct LoaderAnon* loader, char* info_str, struct GVillagerStateTableInfo* info_array, size_t count, bool write, struct LHFile* file) asm("??$LoadIt@VGVillagerStateTableInfo@@@@YAXPAVLoaderAnon@@PADPAVGVillagerStateTableInfo@@K_NPAVLHFile@@@Z");
+void __cdecl LoadIt__FP10LoaderAnonPcP23GVillagerStateTableInfoUlbP6LHFile(struct LoaderAnon* loader, char* info_str, struct GVillagerStateTableInfo* info_array, size_t count, bool write, struct LHFile* file) asm("??$LoadIt@VGVillagerStateTableInfo@@@@YAXPAVLoaderAnon@@PADPAVGVillagerStateTableInfo@@K_NPAVLHFile@@@Z");
 
 #endif /* BW1_DECOMP_LOADER_ANON_INCLUDED_H */
