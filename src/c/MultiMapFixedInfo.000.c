585240086b2d73ac74e33ea1a7a94e5328015e3c
Sandy Carter <bwrsandman@gmail.com>
2025-09-14 18:32:12 -0400

    GBaseInfo: Move `rdata$r` to C

---
+++ src/c/MultiMapFixedInfo.000.c
@@ -0,0 +1,13 @@
+#include "MultiMapFixedInfo.h"
+
+__attribute__((section(".rdata$r"), aligned(8)))
+const struct RTTIBaseClassDescriptor __RTTIBaseClassDescriptor__18GMultiMapFixedInfo = {
+    .pTypeDescriptor = &__RTTITypeDescriptor__18GMultiMapFixedInfo,
+    .numContainedBases = 0x00000003,
+    .where = {
+        .mdisp = 0x00000000,
+        .pdisp = 0xFFFFFFFF,
+        .vdisp = 0x00000000,
+    },
+    .attributes = 0x0000000,
+};
