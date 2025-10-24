d99b83138d5dd25ce34922aba78882db2dae2586
Sandy Carter <bwrsandman@gmail.com>
2025-10-23 21:00:28 -0400

    VillagerInfo: Move pulled up function to c asm
    Ran `python src/scripts/asm_to_c/asm_to_c.py text src/asm/unprocessed/runblack.reassemble.0976.00751970-00753c70.asm 1 >> src/c/VillagerInfo.0977.c`

--- src/asm/unprocessed/runblack.reassemble.0976.00751970-00753c70.asm
+++ src/asm/unprocessed/runblack.reassemble.0976.00751970-00753c70.asm
@@ -150,36 +150,6 @@
 .globl ?GetPrayerSite@Villager@@QAE_NXZ
 .globl ?FUN00751d40@Villager@@QAE_NXZ
 
-?Find@GVillagerInfo@@SAPAV1@W4TRIBE_TYPE@@W4VILLAGER_NUMBER@@@Z:
-                                                         {disp8} mov        edx, dword ptr [esp + 0x08]                   // 0x00752650    8b542408
-                         push               esi                                           // 0x00752654    56
-                         {disp8} mov        esi, dword ptr [esp + 0x08]                   // 0x00752655    8b742408
-                         mov                eax, OFFSET _GVillagerInfo_ARRAY_00da6be8     // 0x00752659    b8e86bda00
-                         mov                ecx, 0x00da6de4                               // 0x0075265e    b9e46dda00
-_jmp_addr_0x00752663:    cmp                dword ptr [ecx + -0x08], esi                  // 0x00752663    3971f8
-                         {disp8} jne        _jmp_addr_0x0075266c                          // 0x00752666    7504
-                         cmp                dword ptr [ecx], edx                          // 0x00752668    3911
-                         {disp8} je         _jmp_addr_0x00752681                          // 0x0075266a    7415
-_jmp_addr_0x0075266c:    add                ecx, 0x000003a4                               // 0x0075266c    81c1a4030000
-                         add                eax, 0x000003a4                               // 0x00752672    05a4030000
-                         cmp                ecx, 0x00db9fb4                               // 0x00752677    81f9b49fdb00
-                         .byte              0x72, 0xe4// {disp8} jb _jmp_addr_0x00752663  // 0x0075267d    72e4
-                         xor.s              eax, eax                                      // 0x0075267f    33c0
-_jmp_addr_0x00752681:    pop                esi                                           // 0x00752681    5e
-                         ret                                                              // 0x00752682    c3
-                         nop                                                              // 0x00752683    90
-                         nop                                                              // 0x00752684    90
-                         nop                                                              // 0x00752685    90
-                         nop                                                              // 0x00752686    90
-                         nop                                                              // 0x00752687    90
-                         nop                                                              // 0x00752688    90
-                         nop                                                              // 0x00752689    90
-                         nop                                                              // 0x0075268a    90
-                         nop                                                              // 0x0075268b    90
-                         nop                                                              // 0x0075268c    90
-                         nop                                                              // 0x0075268d    90
-                         nop                                                              // 0x0075268e    90
-                         nop                                                              // 0x0075268f    90
 ?MakeVillagesMeet@Villager@@QAE_NPAV1@W4VILLAGER_STATES@@M@Z:
                          sub                esp, 0x18                                     // 0x00752690    83ec18
                          push               esi                                           // 0x00752693    56
