fc569decc08b855a9d7c4ff77c7687e5b61be820
Sandy Carter <bwrsandman@gmail.com>
2025-11-01 07:11:47 -0400

    Villager: Move unknown global ctr to C asm

--- src/c/Villager.0977.c
+++ src/c/Villager.0977.c
@@ -7772,3 +7772,40 @@ bool32_t __fastcall CheckInteractWithMagicLiving__8VillagerFv(struct Villager* t
 {
     return 0;
 }
+
+void __cdecl globl_ct_0x00757860(void)
+{
+    asm("{disp32} mov       cl, byte ptr [data_bytes + 0x5e6934]");          // 0x00757860    8a0d34c9fa00
+    asm("mov                al, 0x01");                                      // 0x00757866    b001
+    asm("test               al, cl");                                        // 0x00757868    84c8
+    asm("{disp8} jne        _jmp_addr_0x00757874");                          // 0x0075786a    7508
+    asm("or.s               cl, al");                                        // 0x0075786c    0ac8
+    asm("{disp32} mov       byte ptr [data_bytes + 0x5e6934], cl");          // 0x0075786e    880d34c9fa00
+    asm("_jmp_addr_0x00757874:");
+    asm("{disp32} jmp       _crt_global_destruction_register_0x00757880");   // 0x00757874    e907000000
+    __builtin_unreachable();
+}
+
+void __cdecl crt_global_destruction_register_0x00757880(void)
+{
+    asm("push               0x00407870");                                    // 0x00757880    6870784000
+    asm("call               _atexit");                                       // 0x00757885    e8e7ea0600
+    asm("pop                ecx");                                           // 0x0075788a    59
+    asm("ret");                                                              // 0x0075788b    c3
+    __builtin_unreachable();
+}
+
+void __cdecl globl_ct_0x00757890(void)
+{
+    asm("{disp32} jmp       _FUN_007578a0__8VillagerFv");                    // 0x00757890    e90b000000
+    __builtin_unreachable();
+}
+
+void __cdecl FUN_007578a0__8VillagerFv(void)
+{
+    asm("{disp32} fld       dword ptr [rdata_bytes + 0xf193c]");             // 0x007578a0    d9053ca99900
+    asm("{disp32} fmul      dword ptr [rdata_bytes + 0xf1938]");             // 0x007578a6    d80d38a99900
+    asm("{disp32} fstp      dword ptr [data_bytes + 0x3f3dd4]");             // 0x007578ac    d91dd49ddb00
+    asm("ret");                                                              // 0x007578b2    c3
+    __builtin_unreachable();
+}
