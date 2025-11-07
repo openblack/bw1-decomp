0a456405fd6803a43a5e5f1acde8e4dc604805bb
Sandy Carter <bwrsandman@gmail.com>
2025-11-07 14:35:47 -0500

    State Tables: Move bss to type C declaration

---
+++ src/c/unprocessed/bss.StateTables.c
@@ -0,0 +1,4 @@
+#include "LivingAction.h"
+
+struct Living__StateTableEntry VillagerStateTable[0xff]; // [0x343198] 0x009c6000 + 0x343198 = 0x00d09198
+struct Living__StateTableEntry AnimalStateTable[0x35];   // [0x34c108] 0x009c6000 + 0x34c108 = 0x00d12108
