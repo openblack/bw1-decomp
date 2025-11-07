5764658faa3c668bcf87e389fb300648db492618
Sandy Carter <bwrsandman@gmail.com>
2025-11-07 14:35:47 -0500

    State Tables: Move bss to type C declaration

--- src/asm/unprocessed/bss.001.asm
+++
@@ -1,8 +0,0 @@
-.intel_syntax noprefix
-.section .bss
-
-.globl _VillagerStateTable
-.globl _AnimalStateTable
-
-_VillagerStateTable:                       .space 0x8f70                                                           // [0x343198] 0x009c6000 + 0x343198 = 0x00d09198
-_AnimalStateTable:                         .space 0x1dd0                                                           // [0x34c108] 0x009c6000 + 0x34c108 = 0x00d12108
