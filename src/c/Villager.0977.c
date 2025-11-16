3db2ee6c5b483711046878afe1f6811acc2f8b0f
Sandy Carter <bwrsandman@gmail.com>
2025-11-16 00:04:57 -0500

    Villager: Move unknown global ctr to C asm

--- src/c/Villager.0977.c
+++ src/c/Villager.0977.c
@@ -19973,3 +19973,42 @@ bool32_t __fastcall ExitForesting__8VillagerFUc(struct Villager* this, const voi
     asm("ret                0x0004");                                        // 0x0075fe2f    c20400
     __builtin_unreachable();
 }
+
+// Probably VillagerHome.cpp
+
+void __cdecl globl_ct_0x0075fe40(void)
+{
+    asm("{disp32} mov       cl, byte ptr [data_bytes + 0x5e6934]");          // 0x0075fe40    8a0d34c9fa00
+    asm("mov                al, 0x01");                                      // 0x0075fe46    b001
+    asm("test               al, cl");                                        // 0x0075fe48    84c8
+    asm("{disp8} jne        _jmp_addr_0x0075fe54");                          // 0x0075fe4a    7508
+    asm("or.s               cl, al");                                        // 0x0075fe4c    0ac8
+    asm("{disp32} mov       byte ptr [data_bytes + 0x5e6934], cl");          // 0x0075fe4e    880d34c9fa00
+    asm("_jmp_addr_0x0075fe54:");
+    asm("{disp32} jmp       _crt_global_destruction_register_0x0075fe60");   // 0x0075fe54    e907000000
+    __builtin_unreachable();
+}
+
+void __cdecl crt_global_destruction_register_0x0075fe60(void)
+{
+    asm("push               0x00407870");                                    // 0x0075fe60    6870784000
+    asm("call               _atexit");                                       // 0x0075fe65    e827590600
+    asm("pop                ecx");                                           // 0x0075fe6a    59
+    asm("ret");                                                              // 0x0075fe6b    c3
+    __builtin_unreachable();
+}
+
+void __cdecl globl_ct_0x0075fe70(void)
+{
+    asm("{disp32} jmp       _FUN_0075fe80__8VillagerFv");                    // 0x0075fe70    e90b000000
+    __builtin_unreachable();
+}
+
+void __cdecl FUN_0075fe80__8VillagerFv(void)
+{
+    asm("{disp32} fld       dword ptr [rdata_bytes + 0xf1998]");             // 0x0075fe80    d90598a99900
+    asm("{disp32} fmul      dword ptr [rdata_bytes + 0xf1994]");             // 0x0075fe86    d80d94a99900
+    asm("{disp32} fstp      dword ptr [data_bytes + 0x3f3e18]");             // 0x0075fe8c    d91d189edb00
+    asm("ret");                                                              // 0x0075fe92    c3
+    __builtin_unreachable();
+}
