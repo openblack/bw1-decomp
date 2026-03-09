de4125b0ad11c8c67a675288d53cae66f302c69e
Sandy Carter <bwrsandman@gmail.com>
2026-03-08 22:46:00 -0400

    Abode: Work on the larger constructor

--- src/staging/Abode.000.cpp
+++ src/staging/Abode.000.cpp
@@ -1,7 +1,24 @@
 #include "Abode.h"
 #include "Object.h"
+#include "Town.h"
+#include "Game.h"
+
+extern GGame* _game = (GGame*)0x00d0195c;
 
 // win1.41 00401350 mac 1033b330 Abode::Abode(MapCoords const &, GAbodeInfo const *, Town *, float, float, float, int)
-Abode::Abode(const MapCoords& coords, const GAbodeInfo* info, Town* town, float y_angle, float scale, float food, int wood)
+Abode::Abode(const MapCoords& coords, const GAbodeInfo* info, Town* _town, float y_angle, float scale, float food, int wood)
+    : MultiMapFixed(coords, (const GMultiMapFixedInfo*)info, y_angle, scale, food, wood)
 {
+    SetToZero();
+    if (_town)
+    {
+        _town->AddStructureToTown(this);
+        index = town.payload->abode_list.count + 0xff;
+    }
+
+    // _game->map.field_0x8 |= 1;
+    __asm mov eax, _game
+    __asm or BYTE PTR [eax + 0x059bc + 0x4], 1
+
+    FindNearestDrinkingWater(200.0f);
 }
