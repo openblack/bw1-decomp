0e57766f83b466a905a30f56e2227b480cb76cd1
Sandy Carter <bwrsandman@gmail.com>
2026-03-16 16:21:00 -0400

    Mobile: Split asm

--- src/staging/runblack.reassemble.0640.006020e0-00609320.cpp
+++ src/staging/runblack.reassemble.0640.006020e0-00609320.cpp
@@ -1,6 +1,5 @@
 #include "MapCoords.h"
 #include "Utils.h"
-#include "Mobile.h"
 #include "MobileObjectInfo.h"
 #include "MobileObject.h"
 #include "Poo.h"
@@ -8,30 +7,6 @@
 #include "MobileStaticInfo.h"
 #include "MobileStatic.h"
 #include "lionhead/lh3dlib/development/LH3DIsland.h"
-
-// win1.41 00606bf0 mac 103ba690 Mobile::ApplyThisToMapCoord(GInterfaceStatus *, MapCoords const &, GestureSystemPacketData *)
-uint32_t Mobile::ApplyThisToMapCoord(GInterfaceStatus* param_1, const MapCoords* param_2, GestureSystemPacketData* param_3)
-{
-    return 0;
-}
-
-// win1.41 00606c10 mac 103ba5b0 Mobile::Save(GameOSFile &)
-bool32_t Mobile::Save(GameOSFile& file)
-{
-    return 0;
-}
-
-// win1.41 00606c70 mac 103ba4e0 Mobile::Load(GameOSFile &)
-bool32_t Mobile::Load(GameOSFile& file)
-{
-    return 0;
-}
-
-// win1.41 00606cd0 mac 103ba4a0 Mobile::BlocksTownClearArea( const(void))
-bool32_t Mobile::BlocksTownClearArea() const
-{
-    return 0;
-}
 
 // win1.41 00606da0 mac 100ad7d0 GMobileObjectInfo::GetBaseInfo(unsigned long &)
 GBaseInfo* GMobileObjectInfo::GetBaseInfo(uint32_t& param_1)
