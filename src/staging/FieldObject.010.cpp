136fcc3e92959eaa6de3ef0c14a380515c5b6a7c
Sandy Carter <bwrsandman@gmail.com>
2026-03-16 16:39:05 -0400

    FieldCrop: Split asm

---
+++ src/staging/FieldObject.010.cpp
@@ -0,0 +1,105 @@
+#include "FieldCrop.h"
+#include "Poo.h"
+
+// win1.41 00607dc0 mac 103bb620 FieldCrop::GetHoldType(void)
+HOLD_TYPE FieldCrop::GetHoldType()
+{
+    return (HOLD_TYPE)0;
+}
+
+// win1.41 00607e20 mac 103bb470 FieldCrop::ToBeDeleted(int)
+void FieldCrop::ToBeDeleted(int param_1)
+{
+}
+
+// win1.41 006081e0 mac 103bb330 FieldCrop::SetLife(float)
+void FieldCrop::SetLife(float param_1)
+{
+}
+
+// win1.41 00608250 mac 103bb290 FieldCrop::RemoveMapObject(void)
+void FieldCrop::RemoveMapObject()
+{
+}
+
+// win1.41 00608260 mac 103bb240 FieldCrop::InsertMapObject(void)
+void FieldCrop::InsertMapObject()
+{
+}
+
+// win1.41 00608270 mac 103bb1e0 FieldCrop::IsFunctional(void)
+bool32_t FieldCrop::IsFunctional()
+{
+    return 0;
+}
+
+// win1.41 006082b0 mac 103bb140 FieldCrop::ApplyThisToMapCoord(GInterfaceStatus *, MapCoords const &, GestureSystemPacketData *)
+uint32_t FieldCrop::ApplyThisToMapCoord(GInterfaceStatus* param_1, const MapCoords* param_2, GestureSystemPacketData* param_3)
+{
+    return 0;
+}
+
+// win1.41 006082d0 mac 103bb100 FieldCrop::CanBecomeAPhysicsObject(void)
+bool32_t FieldCrop::CanBecomeAPhysicsObject()
+{
+    return 0;
+}
+
+// win1.41 006082e0 mac 103bb0c0 FieldCrop::GetPhysicsConstantsType(void)
+uint32_t FieldCrop::GetPhysicsConstantsType()
+{
+    return 0;
+}
+
+// win1.41 006082f0 mac 103bafd0 FieldCrop::SetUpPhysOb(PhysOb *)
+void FieldCrop::SetUpPhysOb(PhysOb* param_1)
+{
+}
+
+// win1.41 00608340 mac 103baf80 FieldCrop::InteractsWithPhysicsObjects(void)
+bool32_t FieldCrop::InteractsWithPhysicsObjects()
+{
+    return 0;
+}
+
+// win1.41 00608350 mac 103baf40 FieldCrop::IsARootedObject(void)
+bool32_t FieldCrop::IsARootedObject()
+{
+    return 0;
+}
+
+// win1.41 00608360 mac 103bae90 FieldCrop::ValidToApplyThisToObject(GInterfaceStatus *, Object *)
+uint32_t FieldCrop::ValidToApplyThisToObject(GInterfaceStatus* param_1, Object* param_2)
+{
+    return 0;
+}
+
+// win1.41 00608390 mac 103badc0 FieldCrop::ApplyThisToObject(GInterfaceStatus *, Object *, GestureSystemPacketData *)
+uint32_t FieldCrop::ApplyThisToObject(GInterfaceStatus* param_1, Object* param_2, GestureSystemPacketData* param_3)
+{
+    return 0;
+}
+
+// win1.41 006083c0 mac 103bad80 Poo::GetScriptObjectType(void)
+uint32_t Poo::GetScriptObjectType()
+{
+    return 0;
+}
+
+// win1.41 006083d0 mac 103bad50 Poo::GetHoldType(void)
+HOLD_TYPE Poo::GetHoldType()
+{
+    return (HOLD_TYPE)0;
+}
+
+// win1.41 006083e0 mac 103bac40 FieldCrop::InterfaceSetInMagicHand(GInterfaceStatus *)
+bool32_t FieldCrop::InterfaceSetInMagicHand(GInterfaceStatus* param_1)
+{
+    return 0;
+}
+
+// win1.41 00608440 mac 103baba0 FieldCrop::CreatureMustAvoid(Creature *)
+bool32_t FieldCrop::CreatureMustAvoid(Creature* param_1)
+{
+    return 0;
+}
