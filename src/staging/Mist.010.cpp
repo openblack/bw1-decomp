cf2baebac73e426ee80d072e596245c95f12463d
Sandy Carter <bwrsandman@gmail.com>
2026-03-16 16:04:34 -0400

    Mist: Split asm

---
+++ src/staging/Mist.010.cpp
@@ -0,0 +1,40 @@
+#include "Mist.h"
+
+// win1.41 00606270 mac 101056c0 Mist::Mist(MapCoords const &, float, unsigned long, float)
+Mist::Mist(MapCoords* coords, float param_3, uint32_t param_4, float param_5)
+{
+}
+
+// win1.41 00606300 mac 10105480 Mist::ToBeDeleted(int)
+void Mist::ToBeDeleted(int param_1)
+{
+}
+
+// win1.41 006067d0 mac 10104c90 Mist::GetDistanceFromObject(MapCoords const &)
+float Mist::GetDistanceFromObject(const MapCoords& param_1)
+{
+    return 0;
+}
+
+// win1.41 00606910 mac 10104af0 Mist::GetScriptObjectType(void)
+uint32_t Mist::GetScriptObjectType()
+{
+    return 0;
+}
+
+// win1.41 00606920 mac 10104980 Mist::Save(GameOSFile &)
+bool32_t Mist::Save(GameOSFile& file)
+{
+    return 0;
+}
+
+// win1.41 00606a10 mac 10104820 Mist::Load(GameOSFile &)
+bool32_t Mist::Load(GameOSFile& file)
+{
+    return 0;
+}
+
+// win1.41 00606af0 mac 101047c0 Mist::ResolveLoad(void)
+void Mist::ResolveLoad()
+{
+}
