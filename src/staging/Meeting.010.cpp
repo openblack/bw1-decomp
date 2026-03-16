0335602228801c4cf6052fe3538a596f34589eec
Sandy Carter <bwrsandman@gmail.com>
2026-03-16 15:58:05 -0400

    Meeting: Split asm

---
+++ src/staging/Meeting.010.cpp
@@ -0,0 +1,31 @@
+#include "Meeting.h"
+
+#include "Player.h"
+#include "Town.h"
+
+// win1.41 00606140 mac inlined Meeting::GetTown(void)
+Town* Meeting::GetTown()
+{
+    return 0;
+}
+
+// win1.41 00606150 mac inlined Meeting::RemoveDance(void)
+void Meeting::RemoveDance()
+{
+}
+
+// win1.41 00606160 mac inlined Meeting::_dt(void)
+Meeting::~Meeting()
+{
+}
+
+// win1.41 00606180 mac inlined Meeting::ToBeDeleted(int)
+void Meeting::ToBeDeleted(int param_1)
+{
+}
+
+// win1.41 00606230 mac inlined Meeting::GetPlayer(void)
+GPlayer* Meeting::GetPlayer()
+{
+    return 0;
+}
