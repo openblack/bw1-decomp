f9a2fd1420554707d3a2b3c6db8371ac37339397
Sandy Carter <bwrsandman@gmail.com>
2026-03-21 13:29:40 -0400

    MobileObject: Implement ToBeDeleted

--- libs/lionhead/lhlib/ver5.0/LHLinkedList.h
+++ libs/lionhead/lhlib/ver5.0/LHLinkedList.h
@@ -1,32 +1,6 @@
 #ifndef BW1_DECOMP_LH_LINKED_LIST_INCLUDED_H
 #define BW1_DECOMP_LH_LINKED_LIST_INCLUDED_H
 
-/// Every template of the form
-///   template<typename T>
-///   class LHLinkedNode<T> {
-///     LHFastPointer<LHLinkedNode<T>> next;
-///     T* payload;
-///   };
-///   template<typename T>
-///   class LHLinkedList<T> {
-///     LHFastPointer<LHLinkedNode<T>> head;
-///     uint32_t count;
-///     inline LHLinkedNode<T>* GetStart()
-///     {
-///       return head.get();
-///     }
-///     inline bool Add(T* val)
-///     {
-///       if (!val) return false;
-///       LHLinkedNode<T>* node = new LHLinkedNode<T>(val);
-///       node->next = head;
-///       head = node;
-///       ++count;
-///       return true;
-///     }
-///   };
-/// has the same structure
-
 #include <stdbool.h> /* For bool */
 #include <stdint.h> /* For uint32_t */
 #include <stdlib.h> /* For malloc */
@@ -35,11 +9,13 @@
 
 template<typename T>
 class LHLinkedNode {
+public:
     LHLinkedNode<T>* next;
     T* payload;
 };
 template<typename T>
 class LHLinkedList {
+public:
     LHLinkedNode<T>* head;
     uint32_t count;
     inline LHLinkedNode<T>* GetStart()
@@ -54,6 +30,40 @@ class LHLinkedList {
         head = node;
         ++count;
         return true;
+    }
+    inline void Remove(T* val)
+    {
+        LHLinkedNode<T>* prev = NULL;
+        LHLinkedNode<T>* node = head;
+        while (node != NULL)
+        {
+            LHLinkedNode<T>* next = node->next;
+            if (node->payload == val)
+            {
+                if (node == head)
+                    head = next;
+                else
+                    prev->next = next;
+                count--;
+                delete node;
+            }
+            else
+            {
+                prev = node;
+            }
+            node = next;
+        }
+    }
+    inline bool Contains(T* val)
+    {
+        for (LHLinkedNode<T>* node = head; node != NULL; node = node->next)
+        {
+            if (node->payload == val)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 };
 
