04e298f473a136ead157bd8a55daebc3a9c8f14d
anon <anon@anon.com>
2026-06-11 20:40:59 +0200

    Black/Persistent.cpp: Matching (4 byte-exact fns; first Matching Black TU)

---
+++ src/Black/Persistent.cpp
@@ -0,0 +1,21 @@
+#include "Persistent.h"
+
+// win1.41 00580a10  Persistent::VirtualFunc(void)
+void Persistent::VirtualFunc()
+{
+}
+
+// win1.41 00580a20  Persistent::OnLoaded(void)
+void Persistent::OnLoaded()
+{
+}
+
+// win1.41 00580a30  Persistent::DefineProperties(PropertyList *)
+void Persistent::DefineProperties(PropertyList* list)
+{
+}
+
+// win1.41 00580a40  Persistent::~Persistent(void)
+Persistent::~Persistent()
+{
+}
