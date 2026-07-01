d5c242cebb287a0364bc78761420ef464c217082
Sandy Carter <bwrsandman@gmail.com>
2026-07-01 17:59:40 -0400

    Belief: Migrate Belief

---
+++ src/Black/Belief.cpp
@@ -0,0 +1,13 @@
+#include "Belief.h"
+
+#include "Player.h"
+
+void GBelief::SetBelief(int index, float value)
+{
+    belief_in_player[index] = belief_in_player_max[index] < value ? belief_in_player_max[index] : value;
+}
+
+void GBelief::SetBeliefInPlayerCap(GPlayer* player, float cap)
+{
+    belief_in_player_max[player->GetPlayerNumber()] = cap;
+}
