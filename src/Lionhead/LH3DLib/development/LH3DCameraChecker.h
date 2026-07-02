dcf0ad854210daf0901418a5843c22f15df01d83
Sandy Carter <bwrsandman@gmail.com>
2026-07-01 20:25:52 -0400

    LH3DCameraChecker: Migrate AdjustCameraPosTarget

--- src/Lionhead/LH3DLib/development/LH3DCameraChecker.h
+++ src/Lionhead/LH3DLib/development/LH3DCameraChecker.h
@@ -8,6 +8,9 @@
 
 struct LH3DCameraChecker
 {
+    // BW1W120 0x00eb99a8 BW1M100 0x106f4814 LH3DCameraChecker::g_first
+    static LH3DCameraChecker* g_first;
+
     LH3DCameraChecker* next; /* 0x0 */
     float max_dist;
     LHPoint point;
@@ -20,6 +23,11 @@ struct LH3DCameraChecker
 
     // BW1W120 00821050 BW1M100 100c4df0 LH3DCameraChecker::Create(float, LHPoint, float, long, bool)
     static void Create(float max_distance, LHPoint point, float param_3, long param_4, bool y_only);
+
+    // Non-virtual methods
+
+    // BW1W120 00437e70 BW1M100 00437e70 LH3DCameraChecker::AdjustCameraPosTarget(LHPoint*, LHPoint*)
+    void AdjustCameraPosTarget(LHPoint* pos, LHPoint* target);
 };
 
 #endif /* BW1_DECOMP_LH3D_CAMERA_CHECKER_INCLUDED_H */
