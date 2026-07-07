b86ca256c656e2823017895f208087ef6e3d64b7
Matt Stevens <matt@mattstevens.co.uk>
2026-07-07 11:45:01 +0100

    Add LHDir struct use it in LHOSFile::ConvertDirInfo and LHOSFile::Dir* methods, a few full matches and mostly matching

---
+++ src/Lionhead/LHFile/ver3.0/LHDir.h
@@ -0,0 +1,40 @@
+#ifndef BW1_DECOMP_LH_DIR_INCLUDED_H
+#define BW1_DECOMP_LH_DIR_INCLUDED_H
+
+#include <assert.h> /* For static_assert */
+#include <stdint.h> /* For uint32_t */
+
+#include <windows.h> /* For HANDLE, WIN32_FIND_DATAA */
+
+// A date/time broken out from a DOS date/time word pair (see
+// LHOSFile::ConvertDirInfo). day_of_week and the trailing sub-second field are
+// zeroed rather than derived from the find data.
+struct LHDirDateTime
+{
+    uint8_t day;         /* 0x0 */
+    uint8_t month;       /* 0x1 */
+    uint16_t year;       /* 0x2 */
+    uint8_t day_of_week; /* 0x4 */
+    uint8_t field_0x5;   /* 0x5 */
+    uint8_t hour;        /* 0x6 */
+    uint8_t minute;      /* 0x7 */
+    uint8_t second;      /* 0x8 */
+    uint8_t field_0x9;   /* 0x9 */
+};
+static_assert(sizeof(LHDirDateTime) == 0xa, "Data type is of wrong size");
+
+struct LHDir
+{
+    char name[260];                    /* 0x0 */
+    char alternate_name[14];           /* 0x104 */
+    uint32_t attributes;               /* 0x114 */
+    uint32_t size;                     /* 0x118 */
+    struct LHDirDateTime creation_time; /* 0x11c */
+    struct LHDirDateTime write_time;    /* 0x126 */
+    HANDLE find_handle;                /* 0x130 */
+    uint32_t search_attributes;        /* 0x134 */
+    WIN32_FIND_DATAA find_data;        /* 0x138 */
+};
+static_assert(sizeof(LHDir) == 0x278, "Data type is of wrong size");
+
+#endif /* BW1_DECOMP_LH_DIR_INCLUDED_H */
