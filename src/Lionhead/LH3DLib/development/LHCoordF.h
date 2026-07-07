99e5bdd4e64cd052193a567ea9359c64d8e73024
Matt Stevens <matt@mattstevens.co.uk>
2026-07-07 15:59:13 +0100

    BW1W120: LHScreen symbols

---
+++ src/Lionhead/LH3DLib/development/LHCoordF.h
@@ -0,0 +1,10 @@
+#ifndef BW1_DECOMP_LH_COORD_F_INCLUDED_H
+#define BW1_DECOMP_LH_COORD_F_INCLUDED_H
+
+struct LHCoordF
+{
+    float x; /* 0x0 */
+    float y;
+};
+
+#endif /* BW1_DECOMP_LH_COORD_F_INCLUDED_H */
--- src/Lionhead/LH3DLib/development/LHRegion.h
+++ src/Lionhead/LH3DLib/development/LHRegion.h
@@ -5,10 +5,25 @@
 
 #include "LHCoord.h" /* For struct LHCoord */
 
+// Forward Declares
+
+struct tagRECT;
+
 struct LHRegion
 {
   struct LHCoord start;  /* 0x0 */
   struct LHCoord end;
+
+  // Non-virtual methods
+
+  // BW1W120 007deab0 LHRegion::CoordInRegion(LHCoord const &) const
+  int CoordInRegion(const LHCoord& coord) const;
+  // BW1W120 007deae0 LHRegion::CentreCoord(LHCoord *) const
+  void CentreCoord(LHCoord* out) const;
+  // BW1W120 007deb20 LHRegion::BoundWithRegion(LHRegion *)
+  void BoundWithRegion(LHRegion* other);
+  // BW1W120 007ded80 LHRegion::GetRect(tagRECT *) const
+  void GetRect(tagRECT* out) const;
 };
 static_assert(sizeof(LHRegion) == 0x10, "Data type is of wrong size");
 
