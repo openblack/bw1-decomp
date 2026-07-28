bddca50149f31c08bc49518c5097503bc20ba3be
Sandy Carter <bwrsandman@gmail.com>
2026-07-27 21:13:34 -0400

    libcmt: Match a_loc.obj

--- configure.py
+++ configure.py
@@ -1315,7 +1315,7 @@ config.libs = [
             LibObject(Matching, "libcmt", "..\\build\\intel\\mt_obj\\constpow.obj", progress_category="sdk"),
             LibObject(NonMatching, "libcmt", "build\\intel\\mt_obj\\txtmode.obj", progress_category="sdk"),
             LibObject(NonMatching, "libcmt", "build\\intel\\mt_obj\\w_loc.obj", progress_category="sdk"),
-            LibObject(NonMatching, "libcmt", "build\\intel\\mt_obj\\a_loc.obj", progress_category="sdk"),
+            LibObject(Matching, "libcmt", "build\\intel\\mt_obj\\a_loc.obj", progress_category="sdk"),
             LibObject(Matching, "libcmt", "build\\intel\\mt_obj\\setmode.obj", progress_category="sdk"),
 
             Object(NonMatching, "Lionhead/LHLib/ver5.0/LHWin.cpp", progress_category="sdk"),
