d72e93abfefcd8abf5c4e41dd3d89db1a6138914
Sandy Carter <bwrsandman@gmail.com>
2026-08-04 21:50:10 -0400

    Object: Reverse SetFocus

--- src/Black/Object.cpp
+++ src/Black/Object.cpp
@@ -1,4 +1,19 @@
 #include "Object.h"
 
 #include "ColourConstants.h"    /* For White */
+#include "Landscape.h"          /* For GLandscape */
 #include "LandscapeConstants.h" /* For CellSizeXGridDim */
+
+#include <Lionhead/LH3DLib/development/LH3DMath.h> /* For LH3DMath */
+#include <Lionhead/LH3DLib/development/LHPoint.h>  /* For struct LHPoint */
+
+// BW1W120 006393a0 BW1M100 100299c0 Object::SetFocus(LHPoint const &)
+void Object::SetFocus(const LHPoint* focus)
+{
+	LHPoint pos;
+	float   angle;
+
+	GLandscape::ConvertMapCoordToLandscapePoint(Pos, pos);
+	angle = LH3DMath::GetYAngle(&pos, (LHPoint*)focus);
+	SetYAngle(angle);
+}
