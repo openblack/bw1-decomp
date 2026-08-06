f4dc8581ad8cc522a376b3e24f5aa580021a1035
Sandy Carter <bwrsandman@gmail.com>
2026-08-05 22:39:05 -0400

    LH3DMath: Implement GetYAngle

--- src/Lionhead/LH3DLib/development/LH3DMath.cpp
+++ src/Lionhead/LH3DLib/development/LH3DMath.cpp
@@ -1,1 +1,27 @@
 #include "LH3DMath.h"
+
+#include <math.h> /* For atan2 */
+
+#include "LHPoint.h" /* For struct LHPoint */
+
+// BW1W120 00841290 LH3DMath::GetYAngle(LHPoint *)
+float LH3DMath::GetYAngle(LHPoint* point)
+{
+	float angle = (float)atan2(point->z, point->x);
+	if (angle < 0.0f)
+	{
+		angle += TWO_PI;
+	}
+	return angle;
+}
+
+// BW1W120 00841260 LH3DMath::GetYAngle(LHPoint *, LHPoint *)
+float LH3DMath::GetYAngle(LHPoint* from, LHPoint* to)
+{
+	float angle = (float)atan2(to->z - from->z, to->x - from->x);
+	if (angle < 0.0f)
+	{
+		angle += TWO_PI;
+	}
+	return angle;
+}
