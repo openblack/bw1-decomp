24bc07a5416c63266038871fe653d6f0ccc48d59
Sandy Carter <bwrsandman@gmail.com>
2026-08-07 22:44:25 -0400

    Script: Reverse script function table

--- src/Black/Object.cpp
+++ src/Black/Object.cpp
@@ -4,8 +4,10 @@
 #include "Landscape.h"          /* For GLandscape */
 #include "LandscapeConstants.h" /* For CellSizeXGridDim */
 
-#include <Lionhead/LH3DLib/development/LH3DMath.h> /* For LH3DMath */
-#include <Lionhead/LH3DLib/development/LHPoint.h>  /* For struct LHPoint */
+#include <Lionhead/LH3DLib/development/LH3DIsland.h>    /* For LH3DIsland */
+#include <Lionhead/LH3DLib/development/LH3DMapCoords.h> /* For struct LH3DMapCoords */
+#include <Lionhead/LH3DLib/development/LH3DMath.h>      /* For LH3DMath */
+#include <Lionhead/LH3DLib/development/LHPoint.h>       /* For struct LHPoint */
 
 // BW1W120 006393a0 BW1M100 100299c0 Object::SetFocus(LHPoint const &)
 void Object::SetFocus(const LHPoint* focus)
