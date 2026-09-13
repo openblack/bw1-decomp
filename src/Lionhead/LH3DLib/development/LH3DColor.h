0e18491f1d9874fbe4d7bd19aee4c98e0ec6c745
Matt Stevens <matt@mattstevens.co.uk>
2026-09-13 11:43:28 +0100

    GGame::Loop 100% match - LH3DColor has a default constructor, FrontEnd members are independent globals

--- src/Lionhead/LH3DLib/development/LH3DColor.h
+++ src/Lionhead/LH3DLib/development/LH3DColor.h
@@ -13,6 +13,8 @@ struct LH3DColor
 
 	// Constructors
 
+	// BW1W120 inlined BW1M100 10147d00 LH3DColor::LH3DColor(void)
+	LH3DColor() {}
 	// BW1W120 inlined BW1M100 10083c40 LH3DColor::LH3DColor(uint)
 	// White's per-TU startup initialisers store the packed color as one DWORD.
 	LH3DColor(uint32_t color) { *(uint32_t*)this = color; }
