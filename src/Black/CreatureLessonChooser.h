8adf1bceaaeb093f918f6a8d416f8d3957d30f7d
Sandy Carter <bwrsandman@gmail.com>
2026-09-13 22:02:05 -0400

    CreatureMental: Fix data boundaries and symbols

--- src/Black/CreatureLessonChooser.h
+++ src/Black/CreatureLessonChooser.h
@@ -19,9 +19,4 @@ enum LESSON_TYPE
 };
 static_assert(sizeof(enum LESSON_TYPE) == 0x4, "Data type is of wrong size");
 
-static const char* LESSON_TYPE_strs[_LESSON_TYPE_COUNT] = {
-	"LESSON_TYPE_0x0",
-	"LESSON_TYPE_0x1",
-};
-
 #endif /* BW1_DECOMP_CREATURE_LESSON_CHOOSER_INCLUDED_H */
