acdd852b2bec48ffe359c33c02d86098c954548b
Matt Stevens <matt@mattstevens.co.uk>
2026-09-20 22:41:30 +0100

    This was probably in SetupThing.h too, this regresses on GGame::Loop but it'll fix itself.. at some point.

--- src/Black/SetupThing.h
+++ src/Black/SetupThing.h
@@ -124,4 +124,37 @@ struct SetupThing
 // BW1W120 00407a20 BW1M100 105133e0 GetBigTextSize(void)
 int GetBigTextSize();
 
+// BW1W120 004132c0 BW1M100 1035b610 SetupThing::DrawBox(int, int, int, int, unsigned long, unsigned long, unsigned long, unsigned long, unsigned long, unsigned long)
+inline void SetupThing::DrawBox(int x_min, int y_min, int x_max, int y_max, unsigned long color_1,
+                                unsigned long color_2, unsigned long color_3, unsigned long color_4,
+                                unsigned long use_alpha, unsigned long adjust)
+{
+	if (x_max < x_min)
+	{
+		x_min ^= x_max;
+		x_max ^= x_min;
+		x_min ^= x_max;
+		color_1 ^= color_2;
+		color_2 ^= color_1;
+		color_1 ^= color_2;
+		color_3 ^= color_4;
+		color_4 ^= color_3;
+		color_3 ^= color_4;
+	}
+	if (y_max < y_min)
+	{
+		y_min ^= y_max;
+		y_max ^= y_min;
+		y_min ^= y_max;
+		color_1 ^= color_3;
+		color_3 ^= color_1;
+		color_1 ^= color_3;
+		color_2 ^= color_4;
+		color_4 ^= color_2;
+		color_2 ^= color_4;
+	}
+	DrawQuad(x_min, y_min, x_max, y_min, x_max, y_max, x_min, y_max, color_1, color_2, color_3, color_4, use_alpha,
+	         adjust);
+}
+
 #endif /* BW1_DECOMP_SETUP_THING_INCLUDED_H */
