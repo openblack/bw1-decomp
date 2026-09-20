76474ca864d7ab585c061b0efcd26d82ea3e9892
Matt Stevens <matt@mattstevens.co.uk>
2026-09-20 13:28:02 +0100

    HelpDude: Recover Mac declaration provenance

--- src/Black/HelpDude.h
+++ src/Black/HelpDude.h
@@ -28,7 +28,7 @@ struct HoverZone
 	float InnerRadius;
 	float OuterRadius;
 
-	// BW1W120 005b9620. Mac: Feel__9HoverZoneFff. Windows returns ST(0), not void.
+	// BW1W120 005b9620 BW1M100 103485d0 HoverZone::Feel(float, float)
 	float Feel(float x, float y);
 };
 
@@ -39,9 +39,9 @@ struct EyePositions
 	float field_0x10[2];
 	float Closure[2];
 
-	// BW1W120 005c2040; constructor ownership inferred from HelpDudeEmotion construction.
+	// BW1W120 005c2040 EyePositions::EyePositions(void)
 	EyePositions();
-	// BW1W120 005ba9c0. Descriptive method name; no separate Mac symbol located.
+	// BW1W120 005ba9c0 void EyePositions::Interp(EyePositions*, EyePositions*, float)
 	void Interp(EyePositions* start, EyePositions* end, float amount);
 };
 
@@ -51,9 +51,9 @@ struct EyeBlinker
 	float MaxDelay;
 	float Duration;
 
-	// BW1W120 005c2070; ownership corroborated by EyeBlinker::Update at 005bac10.
+	// BW1W120 005c2070 EyeBlinker::EyeBlinker(void)
 	EyeBlinker();
-	// BW1W120 005baad0. Descriptive name; no separate Mac symbol located.
+	// BW1W120 005baad0 void EyeBlinker::Interp(EyeBlinker*, EyeBlinker*, float)
 	void Interp(EyeBlinker* start, EyeBlinker* end, float amount);
 };
 
@@ -67,9 +67,9 @@ struct HelpDudeEmotion
 	float        field_0x38;
 	float        field_0x3c;
 
-	// BW1W120 005c2090. Mac: __ct__15HelpDudeEmotionFv.
+	// BW1W120 005c2090 BW1M100 1033f540 HelpDudeEmotion::HelpDudeEmotion(void)
 	HelpDudeEmotion();
-	// BW1W120 005baa40. Mac: Interp__15HelpDudeEmotionFP15HelpDudeEmotionP15HelpDudeEmotionf.
+	// BW1W120 005baa40 BW1M100 10347bc0 HelpDudeEmotion::Interp(HelpDudeEmotion*, HelpDudeEmotion*, float)
 	void Interp(HelpDudeEmotion* start, HelpDudeEmotion* end, float amount);
 };
 
@@ -180,99 +180,91 @@ public:
 
 	// Override methods
 
-	// BW1W120 005c2110 BW1M100 10348c90 HelpDude::_dt(void)
+	// BW1W120 inlined BW1M100 10348c90 HelpDude::~HelpDude(void)
 	virtual ~HelpDude();
-	// BW1W120 005c20e0 BW1M100 10348c40 HelpDude::Get3DSoundPos(LHPoint *)
+	// BW1W120 005c20e0 BW1M100 10348c40 HelpDude::Get3DSoundPos(LHPoint*)
 	virtual int Get3DSoundPos(LHPoint* param_1);
 
 	// Windows addresses; method names and parameter lists corroborated by embedded Mac symbols.
-	// BW1W120 005b96d0
+	// BW1W120 005b96d0 BW1M100 10348530 HelpDude::Sethoverx(float, float, bool)
 	void Sethoverx(float x, float time, bool clamp);
-	// BW1W120 005b98d0
+	// BW1W120 005b98d0 BW1M100 10348490 HelpDude::Sethovery(float, float, bool)
 	void Sethovery(float y, float time, bool clamp);
-	// BW1W120 005b9ad0. Windows returns ST(0), not the void in the current symbol map.
+	// BW1W120 005b9ad0 BW1M100 10348350 HelpDude::Feel(float, float)
 	float Feel(float x, float y);
-	// BW1W120 005ba010
+	// BW1W120 005ba010 BW1M100 10348180 HelpDude::UpdateHoverZ(float)
 	void UpdateHoverZ(float min_z);
-	// BW1W120 005bb730
+	// BW1W120 005bb730 BW1M100 10015e50 HelpDude::IsTalkingWithDelay(void)
 	int IsTalkingWithDelay();
-	// BW1W120 005bb760. Windows returns a full-register Boolean.
-	int IsTalking(); // TODO: Audio-system declarations and shared speech storage are unrecovered.
-	// BW1W120 005bb8f0. Descriptive name; no separate Mac symbol located.
+	// BW1W120 005bb760 BW1M100 10015d60 HelpDude::IsTalking(void)
+	int IsTalking();
+	// BW1W120 005bb8f0 void HelpDude::CheckSmokeTrigger(float)
 	void CheckSmokeTrigger(float position);
-	// BW1W120 005bbbd0
+	// BW1W120 005bbbd0 BW1M100 10346d20 HelpDude::SetPos(const LHCoord&)
 	void SetPos(const LHCoord& position);
-	// BW1W120 005bbcd0
+	// BW1W120 005bbcd0 BW1M100 10346c50 HelpDude::ResetTrail(void)
 	void ResetTrail();
-	// BW1W120 005bbd20
+	// BW1W120 005bbd20 BW1M100 10346b80 HelpDude::UpdateClingEdge(void)
 	void UpdateClingEdge();
-	// BW1W120 005bbdd0
+	// BW1W120 005bbdd0 BW1M100 10346a30 HelpDude::FlyTo2D(const LHCoord&, float, bool)
 	void FlyTo2D(const LHCoord& position, float time, bool clamp);
-	// BW1W120 005bbe70
+	// BW1W120 005bbe70 BW1M100 10346980 HelpDude::SetCling(float, float, bool)
 	void SetCling(float x, float y, bool instant);
-	// BW1W120 005bbef0. Windows returns AL, unlike IsTalking.
+	// BW1W120 005bbef0 BW1M100 103468f0 HelpDude::IsAnimPlaying(void)
 	bool IsAnimPlaying();
-	// BW1W120 005bbf30
+	// BW1W120 005bbf30 BW1M100 103467f0 HelpDude::PlayAnimAtPos(float, float, ANIMLIST, float)
 	void PlayAnimAtPos(float x, float y, ANIMLIST anim, float speed);
-	// BW1W120 005bbfa0
+	// BW1W120 005bbfa0 BW1M100 10346700 HelpDude::PointAt2D(const LHCoord&)
 	void PointAt2D(const LHCoord& position);
-	// BW1W120 005bc6c0
+	// BW1W120 005bc6c0 BW1M100 10345f60 HelpDude::LookAt(LHPoint*)
 	void LookAt(LHPoint* point);
-	// BW1W120 005bc700
+	// BW1W120 005bc700 BW1M100 10345ef0 HelpDude::LookAtCamera(void)
 	void LookAtCamera();
-	// BW1W120 005bc720
+	// BW1W120 005bc720 BW1M100 10345e70 HelpDude::LookAtOtherGuy(void)
 	void LookAtOtherGuy();
-	// BW1W120 005bc750
+	// BW1W120 005bc750 BW1M100 10345e10 HelpDude::EyeLookAt(LHPoint*)
 	void EyeLookAt(LHPoint* point);
-	// BW1W120 005bc790 / 005bc7a0. Descriptive names; no separate Mac symbols located.
+	// BW1W120 005bc790 void HelpDude::EyeLookAtCamera(void)
 	void EyeLookAtCamera();
+	// BW1W120 005bc7a0 void HelpDude::EyeLookAtOtherGuy(void)
 	void EyeLookAtOtherGuy();
-	// BW1W120 005bc7c0. Windows returns an audio-bank pointer, not void.
+	// BW1W120 005bc7c0 BW1M100 10345dd0 HelpDude::GetSoundFXBank(void)
 	LH_AudioBank* GetSoundFXBank();
-	// BW1W120 005bcc20
+	// BW1W120 005bcc20 BW1M100 10345730 HelpDude::ResetLipSyncAnimList(void)
 	void ResetLipSyncAnimList();
-	// BW1W120 005bcc90
+	// BW1W120 005bcc90 BW1M100 10345670 HelpDude::FlyToGimme(void)
 	void FlyToGimme();
-	// BW1W120 005bd210
+	// BW1W120 005bd210 BW1M100 10344ea0 HelpDude::SetEmotion(int, float)
 	void SetEmotion(int emotion, float time);
-	// BW1W120 005bd250. Windows uses caller cleanup and does not read this.
+	// BW1W120 005bd250 BW1M100 10344e00 HelpDude::Smoothify(float)
 	static float Smoothify(float amount);
-	// BW1W120 005bd440. Hidden LHCoord return buffer; caller cleanup.
+	// BW1W120 005bd440 BW1M100 10344990 HelpDude::GetHomeIdx(int)
 	static LHCoord GetHomeIdx(int edge);
-	// BW1W120 005bdd40
+	// BW1W120 005bdd40 BW1M100 10344080 HelpDude::UpdateHoverZones(float, int)
 	void UpdateHoverZones(float time, int param_2);
-	// BW1W120 005bf5d0
+	// BW1W120 005bf5d0 BW1M100 10342480 HelpDude::Update(float, int, float, int, int)
 	void Update(float time, int param_2, float param_3, int param_4, int param_5);
-	// BW1W120 005bf620
+	// BW1W120 005bf620 BW1M100 103422b0 HelpDude::Update2(float, int, float, int, int)
 	void Update2(float time, int param_2, float param_3, int param_4, int param_5);
-	// BW1W120 005bf800
+	// BW1W120 005bf800 BW1M100 10342260 HelpDude::FinishUpdate(void)
 	void FinishUpdate();
-	// BW1W120 005c06b0
+	// BW1W120 005c06b0 BW1M100 10341240 HelpDude::SetHoverArea(float, float, float, float, float, int)
 	void SetHoverArea(float x, float y, float inner_radius, float outer_radius, float strength, int index);
 
 	// TODO: Recover the state machine, animation/rendering, resource loading and speech methods.
 	// These are declarations of actual target routines, not fallback implementations.
-	// BW1W120 005bd4a0
+	// BW1W120 005bd4a0 BW1M100 103443e0 HelpDude::SetState(HELPDUDESTATE, int)
 	void SetState(HELPDUDESTATE state, int param_2);
-	// BW1W120 005bdda0
+	// BW1W120 005bdda0 BW1M100 10342530 HelpDude::Update1(float, int, float, int, int)
 	void Update1(float time, int param_2, float param_3, int param_4, int param_5);
-	// BW1W120 005bfe00 / 005c0140
+	// BW1W120 005bfe00 BW1M100 10341c40 HelpDude::CalcHeadPos(LHMatrix&, LHPoint&, int)
 	void CalcHeadPos(LHMatrix& matrix, LHPoint& point, int bone);
+	// BW1W120 005c0140 BW1M100 10341850 HelpDude::ApplyHeadMovement(bool)
 	void ApplyHeadMovement(bool pointing);
-	// BW1W120 005c0610
+	// BW1W120 005c0610 BW1M100 103412a0 HelpDude::FinishAnimStack(LHMatrix&)
 	void FinishAnimStack(LHMatrix& matrix);
-	// BW1W120 005c15b0; target map incorrectly splits this routine at 005c15b2.
+	// BW1W120 005c15b0 BW1M100 1033fe10 HelpDude::Uninit(bool)
 	void Uninit(bool reinit);
 };
-
-static_assert(sizeof(HoverZone) == 0x14, "HoverZone size is incorrect");
-static_assert(sizeof(EyePositions) == 0x20, "EyePositions size is incorrect");
-static_assert(sizeof(HelpDudeEmotion) == 0x40, "HelpDudeEmotion size is incorrect");
-static_assert(sizeof(HelpDudeTrail) == 0x34c, "HelpDudeTrail size is incorrect");
-static_assert(sizeof(HelpDude) == 0x37f0, "HelpDude size is incorrect");
-static_assert(offsetof(HelpDude, Trail) == 0x28bc, "HelpDude trail offset is incorrect");
-static_assert(offsetof(HelpDude, Matrix) == 0x3350, "HelpDude matrix offset is incorrect");
-static_assert(offsetof(HelpDude, HoverX) == 0x3514, "HelpDude hover offset is incorrect");
-static_assert(offsetof(HelpDude, SampleOptions) == 0x367c, "HelpDude sample options offset is incorrect");
 
 #endif /* BW1_DECOMP_HELP_DUDE_INCLUDED_H */
